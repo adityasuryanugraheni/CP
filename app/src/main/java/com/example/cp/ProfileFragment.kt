67e4158f8842d4b5f5c076f0fffc0ca41fb78cda
yumnaankyprita <yumnaankyprita@gmail.com>
2025-11-26 14:43:16 +0700

    Commit Profile

--- app/src/main/java/com/example/cp/ProfileFragment.kt
+++ app/src/main/java/com/example/cp/ProfileFragment.kt
@@ -1,17 +1,31 @@
 package com.example.cp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
 class ProfileFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        val btnLogout = view.findViewById<Button>(R.id.btnLogout)
+        btnLogout.setOnClickListener {
+            // Pindah ke LoginActivity saat Logout ditekan
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+        }
+
+        return view
     }
 }
